5bb296ac82c78e282bec6eadc727c34876250688
Loric Brevet <loric.brevet@gmail.com>
2019-12-25 21:40:32 +0100

    Change bootstrap

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./home/main.nix
+    ./home/term.nix
     ./home/gui.nix
   ];
 }
--- home/gui.nix
+++ home/gui.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Allow XDG linking
+  xdg.enable = true;
+
   # Enable xsession
   xsession.enable = true;
 
