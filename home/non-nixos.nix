094e15f3977ee0d9efba3ca03b45f77b5abdb10c
Loric Brevet <loric.brevet@gmail.com>
2020-01-21 23:15:43 +0100

    Correct mistake on path that prevents from booting user session

--- home/non-nixos.nix
+++ home/non-nixos.nix
@@ -9,7 +9,7 @@
       fi
 
       # Set xdg path that is normally set in NixOS with /etc/profile
-      [[ ":$XDG_DATA_DIRS:" != *":$HOME/.nix-profile/share:"* ]] && PATH="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
-      [[ ":$XDG_CONFIG_DIRS:" != *":$HOME/.nix-profile/etc/xdg:"* ]] && PATH="$HOME/.nix-profile/etc/xdg:$XDG_CONFIG_DIRS"
+      [[ ":$XDG_DATA_DIRS:" != *":$HOME/.nix-profile/share:"* ]] && export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
+      [[ ":$XDG_CONFIG_DIRS:" != *":$HOME/.nix-profile/etc/xdg:"* ]] && export XDG_CONFIG_DIRS="$HOME/.nix-profile/etc/xdg:$XDG_CONFIG_DIRS"
   '';
 }
