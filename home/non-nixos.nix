cc801fb081b179c974b5310f8e876769de91b020
Loric Brevet <loric.brevet@amersports.com>
2020-01-23 12:47:58 +0100

    /home/brevetl/bin not in PATH for non-NixOS setup

--- home/non-nixos.nix
+++ home/non-nixos.nix
@@ -8,6 +8,9 @@
           source $HOME/.nix-profile/etc/profile.d/nix.sh
       fi
 
+      # Add bin in PATH if not already existing
+      [[ ":$PATH:" != *":$HOME/bin:"* ]] && export PATH="$PATH:$HOME/bin"
+
       # Set xdg path that is normally set in NixOS with /etc/profile
       [[ ":$XDG_DATA_DIRS:" != *":$HOME/.nix-profile/share:"* ]] && export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
       [[ ":$XDG_CONFIG_DIRS:" != *":$HOME/.nix-profile/etc/xdg:"* ]] && export XDG_CONFIG_DIRS="$HOME/.nix-profile/etc/xdg:$XDG_CONFIG_DIRS"
