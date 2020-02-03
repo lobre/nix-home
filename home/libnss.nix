09359a56973f2296fbc0dd97ac8db8df731954f3
Loric Brevet <loric.brevet@gmail.com>
2020-02-03 21:28:19 +0100

    Add libnss overlays

---
+++ home/libnss.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  # Fix some packages not finding libnss library
+  nixpkgs.overlays = [
+    (
+      self: super: {
+        kitty = super.writeScriptBin "kitty" ''
+          #!${super.stdenv.shell}
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib/x86_64-linux-gnu
+          exec ${super.kitty}/bin/kitty "$@"
+        '';
+      }
+    )
+    (
+      self: super: {
+        i3lock-fancy = super.writeScriptBin "i3lock-fancy" ''
+          #!${super.stdenv.shell}
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib/x86_64-linux-gnu
+          exec ${super.i3lock-fancy}/bin/i3lock-fancy "$@"
+        '';
+      }
+    )
+  ];
+
+}
+
