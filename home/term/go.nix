78b47e037859b19df175d012cd3580f194dc27cb
Loric Brevet <loric.brevet@amersports.com>
2020-03-19 13:55:23 +0100

    Add /home/brevetl/Lab/go/bin in path

--- home/term/go.nix
+++ home/term/go.nix
@@ -8,5 +8,10 @@
   };
 
   home.sessionVariables.GOROOT = "${pkgs.go}/share/go";
+
+  programs.bash.profileExtra = ''
+      # Add go bin in PATH if not already existing
+      [[ ":$PATH:" != *":$GOPATH/bin:"* ]] && export PATH="$PATH:$GOPATH/bin"
+  '';
 }
 
