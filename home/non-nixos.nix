535f17bfe40c93411f1303e3bef24a834aa3eccb
Loric Brevet <loric.brevet@amersports.com>
2020-10-08 13:37:04 +0200

    Simplify non-nixos hacks by using the new hm parameter

--- home/non-nixos.nix
+++ home/non-nixos.nix
@@ -9,16 +9,8 @@ let
 in
 
 {
-  # To correct missing behaviors on non NixOS systems
-  programs.bash.profileExtra = ''
-      # Load nix if not already loaded
-      [[ ":$PATH:" != *":$HOME/.nix-profile/bin:"* ]] && source $HOME/.nix-profile/etc/profile.d/nix.sh
-
-      # Set xdg path that is normally set in NixOS with /etc/profile
-      [[ ":$XDG_DATA_DIRS:" != *":$HOME/.nix-profile/share:"* ]] && export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
-      [[ ":$XDG_CONFIG_DIRS:" != *":$HOME/.nix-profile/etc/xdg:"* ]] && export XDG_CONFIG_DIRS="$HOME/.nix-profile/etc/xdg:$XDG_CONFIG_DIRS"
-  '';
-  programs.zsh.profileExtra = config.programs.bash.profileExtra;
+  # Enable settings that make home manager work better on Linux distribs other than NixOS
+  targets.genericLinux.enable = true;
 
   # To fix glibc locale bug (https://github.com/NixOS/nixpkgs/issues/38991)
   home.sessionVariables.LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
