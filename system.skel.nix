607c1a4513b72a44dd71050c60c4405a7db00dce
Loric Brevet <loric.brevet@amersports.com>
2020-10-24 16:29:24 +0200

    Add configuration skeletons

---
+++ system.skel.nix
@@ -0,0 +1,38 @@
+# This is a configuration skeleton that can act as a starting point 
+# when installing a NixOS system.
+
+{ config, pkgs, ... }:
+
+{
+  imports = 
+    [
+      # results of the hardware scan
+      ./hardware-configuration.nix
+
+      # personal configuration
+      ./system/configuration.nix)
+      ./system/users.nix)
+      #./system/zfs.nix)
+      #./system/x11.nix)
+      #./system/vmware.nix)
+    ];
+
+  # for efi
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  # for bios (replace with disk where grub installed)
+  #boot.loader.grub.device = "/dev/disk/by-id/wwn-0x500001234567890a";
+
+  # networking id needed by zfs (head -c 8 /etc/machine-id)
+  #networking.hostId = "238330f5";
+
+  # hashed passwords of users
+  # :r! mkpasswd -m sha-512
+  # or :term on neovim
+  users.users = {
+    #lobre.hashedPassword = "";
+  };
+
+  system.stateVersion = "20.03";
+}
