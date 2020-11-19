3fc895e1db254f60b64f360706d25fa49482f4b8
Loric <loric.brevet@amersports.com>
2020-11-19 20:44:20 +0100

    Remove pretty much all graphical configuration to simply use xfce

--- home/gui.nix
+++ home/gui.nix
@@ -21,24 +21,13 @@ in
   home.packages = with pkgs; [
     discord
     filezilla
-    firefox 
-    gnome3.eog
-    gnome3.gnome-control-center
     gnome3.gucharmap
     gnome3.meld
-    gnome3.sushi
     google-chrome
     libnotify
-    lxappearance
-    mattermost-desktop
-    networkmanager
-    obsidian
-    pantheon.elementary-files
     pavucontrol
     peek
     remmina
-    shutter
-    skype
     slack-dark
     spotify
     teams
@@ -62,20 +51,4 @@ in
     enable = true;
     platformTheme = "gtk";
   };
-
-  imports = [
-    ./gui/i3.nix
-    ./gui/i3status.nix
-    ./gui/rofi.nix
-    ./gui/greenclip.nix
-    ./gui/alacritty.nix
-    ./gui/xresources.nix
-    ./gui/picom.nix
-    ./gui/dunst.nix
-    ./gui/oni2.nix
-    ./gui/sway.nix
-    ./gui/swaylock.nix
-    ./gui/mako.nix
-    ./gui/wofi.nix
-  ];
 }
