194ea74f5f1ae06b1a232ddca46e88e2f19d5b93
Loric Brevet <loric.brevet@amersports.com>
2020-12-20 18:38:16 +0100

    Keep nixGL just in case for adhoc usecases

--- home/non-nixos.nix
+++ home/non-nixos.nix
@@ -1,6 +1,19 @@
 { config, pkgs, lib, ... }:
 
+let
+  nixGLExpr = fetchTarball "https://github.com/guibou/nixGL/archive/master.tar.gz";
+
+  nixGL = (import "${nixGLExpr}/default.nix" {
+      pkgs = pkgs;
+  }).nixGLDefault;
+in
+
 {
+  # Add nixGL to packages
+  home.packages = [
+    nixGL
+  ];
+
   # Enable settings that make home manager work better on Linux distribs other than NixOS
   targets.genericLinux.enable = true;
 
