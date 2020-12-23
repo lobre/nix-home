68c3c650e6f8d4262f49c61c5838e50a1b61ef47
Loric Brevet <loric.brevet@amersports.com>
2020-12-23 15:38:11 +0100

    Tmux add copy to clipboard

--- home/term/tmux.nix
+++ home/term/tmux.nix
@@ -45,7 +45,13 @@ in
     terminal = "screen-256color";
     keyMode = "vi";
     customPaneNavigationAndResize = true; # remap to use hjkl to switch windows
+    disableConfirmationPrompt = true; # don't prompt when killing pane
     historyLimit = 5000;
     clock24 = true;
+
+    extraConfig = ''
+      set -g mouse on
+      bind -T copy-mode-vi Enter send -X copy-pipe-and-cancel '${pkgs.xclip}/bin/xclip -in -selection clipboard'
+    '';
   };
 }
