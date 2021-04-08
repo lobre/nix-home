f4b92f0637cd37b0e93df9e699cde25f35949bc3
Loric Brevet <loric.brevet@gmail.com>
2021-04-09 00:07:11 +0200

    Add terminal color palette close to the default one but nicer

--- roles/hm/xfce/terminal.nix
+++ roles/hm/xfce/terminal.nix
@@ -1,8 +1,8 @@
 let
-  black="#4B5262";
+  black="#15171C";
   red="#BF616A";
   green="#A3BE8C";
-  orange="#EBCB8B";
+  yellow="#EBCB8B";
   blue="#81A1C1";
   magenta="#B48EAD";
   cyan="#89D0BA";
@@ -11,16 +11,16 @@ let
   brightBlack="#434A5A";
   brightRed="#B3555E";
   brightGreen="#93AE7C";
-  brightOrange="#DBBB7B";
+  brightYellow="#DBBB7B";
   brightBlue="#7191B1";
   brightMagenta="#A6809F";
   brightCyan="#7DBBA8";
   brightWhite="#D1D5DC";
 
-  background="#2F343F";
+  background="#0C0D10";
   foreground="#D8DEE8";
 
-  palette = "${black};${red};${green};${orange};${blue};${magenta};${cyan};${white};${brightBlack};${brightRed};${brightGreen};${brightOrange};${brightBlue};${brightMagenta};${brightCyan};${brightWhite}";
+  palette = "${black};${red};${green};${yellow};${blue};${magenta};${cyan};${white};${brightBlack};${brightRed};${brightGreen};${brightYellow};${brightBlue};${brightMagenta};${brightCyan};${brightWhite}";
 in 
 
 {
@@ -65,5 +65,8 @@ in
     ScrollingUnlimited=TRUE
     ShortcutsNoMnemonics=TRUE
     TitleMode=TERMINAL_TITLE_REPLACE
+    ColorForeground=${foreground}
+    ColorBackground=${background}
+    ColorPalette=${palette}
   '';
 }
