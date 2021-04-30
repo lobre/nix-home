555943e74c1c5188decbf3ca0ea1879359d4b5d3
Loric Brevet <loric.brevet@gmail.com>
2021-04-30 17:45:40 +0200

    Initial version of neovim built in lsp

--- roles/hm/base/vim.nix
+++ roles/hm/base/vim.nix
@@ -1,24 +1,117 @@
 { pkgs, ... }:
 
 {
+  # Use an overlay to install nvim 0.5 until it is not officially shipped with nix.
+  # It will bring pkgs.neovim-nightly in scope.
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
   programs.neovim = {
     enable = true;
+    package = pkgs.neovim-nightly;
 
+    viAlias = true;
     vimAlias = true;
 
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      zig-vim
-      { 
-        plugin = vim-go;
-        config = ''
-          let g:go_fmt_command = "goimports"
-        '';
-      }
+    # Extra packages available to nvim.
+    # Useful to install LSP servers.
+    extraPackages = with pkgs; [ 
+      gopls # go language server
+      nodePackages.bash-language-server
+      nodePackages.dockerfile-language-server-nodejs
+      nodePackages.vim-language-server
+      nodePackages.vscode-css-languageserver-bin
+      nodePackages.vscode-html-languageserver-bin
+      nodePackages.vscode-json-languageserver
+      nodePackages.yaml-language-server
+      rnix-lsp # nix language server
+      zls # zig language server
     ];
 
-    # extra packages available to nvim
-    extraPackages = [];
+    plugins = with pkgs.vimPlugins; [
+      { 
+        plugin = nvim-lspconfig;
+        config = ''
+          lua <<EOF
+          local lspconfig = require('lspconfig')
+
+          local on_attach = function(client, bufnr)
+            local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+            local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+            buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+            -- Mappings.
+            local opts = { noremap=true, silent=true }
+            buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+            buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+            buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+            buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+            buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+            buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+            buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+            buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+            buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+            buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+            buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+            -- Set some keybinds conditional on server capabilities
+            if client.resolved_capabilities.document_formatting then
+              buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+            end
+            if client.resolved_capabilities.document_range_formatting then
+              buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+            end
+
+            -- Set autocommands conditional on server_capabilities
+            if client.resolved_capabilities.document_highlight then
+              vim.api.nvim_exec([[
+                hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+                hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+                hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+                augroup lsp_document_highlight
+                  autocmd! * <buffer>
+                  autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                  autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                augroup END
+              ]], false)
+            end
+          end
+
+          -- Use a loop to conveniently both setup defined servers 
+          -- and map buffer local keybindings when the language server attaches
+          local servers = { "gopls", "bashls", "dockerls", "vimls", "cssls", "html", "jsonls", "yamlls", "rnix", "zls" }
+          for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup { on_attach = on_attach }
+          end
+          EOF
+        '';
+      }
+      { 
+        plugin = nvim-treesitter;
+        config = ''
+          lua <<EOF
+          local treesitter = require'nvim-treesitter.configs'
+          treesitter.setup {
+            ensure_installed = "all",
+            highlight = {
+              enable = true
+            }
+          }
+          EOF
+        '';
+      }
+      vim-nix
+      zig-vim
+    ];
 
     extraConfig = ''
       set showbreak=↪\
@@ -62,7 +155,7 @@
       " Trigger autoread when files changes on disk
       autocmd FocusGained * checktime
 
-      " Reset some colors
+      " Simpler colors for some parts of the interface
       highlight SignColumn ctermbg=none
       highlight Error ctermbg=none ctermfg=red
       highlight Todo ctermbg=none ctermfg=red
