599c636b7b7a69705a6ddf63c329eb0d507b9aa0
Loric Brevet <loric.brevet@gmail.com>
2021-05-08 00:42:47 +0200

    Improve lsp config

--- roles/hm/base/vim.nix
+++ roles/hm/base/vim.nix
@@ -32,6 +32,8 @@
     ];
 
     plugins = with pkgs.vimPlugins; [
+      vim-nix
+      zig-vim
       { 
         plugin = nvim-lspconfig;
         config = ''
@@ -39,36 +41,36 @@
           local lspconfig = require('lspconfig')
 
           local on_attach = function(client, bufnr)
-            local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-            local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+            local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+            local function opt(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-            buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+            opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
             -- Mappings.
             local opts = { noremap=true, silent=true }
-            buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-            buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-            buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-            buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-            buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-            buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-            buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-            buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-            buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-            buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-            buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-            buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-            buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-            buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+            map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+            map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+            map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+            map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+            map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+            map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+            map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+            map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+            map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+            map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+            map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
             -- Set some keybinds conditional on server capabilities
             if client.resolved_capabilities.document_formatting then
-              buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+              map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
             end
             if client.resolved_capabilities.document_range_formatting then
-              buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+              map("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
             end
 
             -- Set autocommands conditional on server_capabilities
@@ -86,11 +88,10 @@
             end
           end
 
-          -- Use a loop to conveniently both setup defined servers 
-          -- and map buffer local keybindings when the language server attaches
+          -- Configure servers
           local servers = { "gopls", "bashls", "dockerls", "vimls", "cssls", "html", "jsonls", "yamlls", "rnix", "zls" }
-          for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup { on_attach = on_attach }
+          for _, server in ipairs(servers) do
+            lspconfig[server].setup { on_attach = on_attach }
           end
           EOF
         '';
@@ -99,9 +100,7 @@
         plugin = nvim-treesitter;
         config = ''
           lua <<EOF
-          local treesitter = require'nvim-treesitter.configs'
-          treesitter.setup {
-            ensure_installed = "all",
+          require'nvim-treesitter.configs'.setup {
             highlight = {
               enable = true
             }
@@ -109,8 +108,6 @@
           EOF
         '';
       }
-      vim-nix
-      zig-vim
     ];
 
     extraConfig = ''
@@ -124,6 +121,9 @@
       set smartindent    " Smart autoindenting when starting new line
       set title          " Update the title of the window 
       set mouse=a        " Enable mouse mode
+
+      " Don’t automatically select first item in completion
+      set completeopt=menuone,noinsert,noselect
 
       " Split as expected
       set splitright
@@ -146,6 +146,9 @@
 
       " Save with sudo
       command! W w !sudo tee % > /dev/null
+
+      " Exit terminal with Esc
+      tnoremap <Esc> <C-\><C-n>
 
       " Explorer settings
       let g:netrw_liststyle=3
