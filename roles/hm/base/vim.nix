14cc1258518ded521278fe94f6e674a41bb5468e
Loric Brevet <loric.brevet@gmail.com>
2021-05-11 00:34:38 +0200

    Finish lsp config

--- roles/hm/base/vim.nix
+++ roles/hm/base/vim.nix
@@ -46,46 +46,28 @@
 
             opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-            -- Mappings.
+            -- Mappings
             local opts = { noremap=true, silent=true }
-            map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
             map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            map('n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
             map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-            map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-            map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-            map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-            map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-            map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-            map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
             map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-            map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-            map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-            map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-            map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+            map('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            map('n', 'gca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+            map('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
-            -- Set some keybinds conditional on server capabilities
-            if client.resolved_capabilities.document_formatting then
-              map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-            end
-            if client.resolved_capabilities.document_range_formatting then
-              map("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-            end
+            -- Commands
+            vim.cmd("command! LspFormat lua vim.lsp.buf.formatting()")
+            vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
+            vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
+            vim.cmd("command! LspDiagList lua vim.lsp.diagnostic.set_loclist()")
 
-            -- Set autocommands conditional on server_capabilities
-            if client.resolved_capabilities.document_highlight then
-              vim.api.nvim_exec([[
-                hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-                hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-                hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-                augroup lsp_document_highlight
-                  autocmd! * <buffer>
-                  autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-                  autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-                augroup END
-              ]], false)
-            end
+            -- Format go files on save
+            vim.api.nvim_exec([[
+              autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)
+            ]], false)
           end
 
           -- Configure servers
@@ -162,8 +144,8 @@
       highlight SignColumn ctermbg=none
       highlight Error ctermbg=none ctermfg=red
       highlight Todo ctermbg=none ctermfg=red
-      highlight Pmenu ctermbg=white
-      highlight PmenuSel ctermbg=gray ctermfg=black
+      highlight Pmenu ctermbg=black ctermfg=white
+      highlight PmenuSel ctermbg=white ctermfg=black
       highlight TabLineFill cterm=bold
       highlight TabLineSel cterm=bold
       highlight TabLine ctermbg=none ctermfg=none cterm=none
