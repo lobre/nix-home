fadbf6f050733963f6618d044e36218e28249fa4
Loric Brevet <loric.brevet@gmail.com>
2021-08-20 15:46:57 +0200

    Nvim enable treesitter indentation

--- roles/hm/base/vim.nix
+++ roles/hm/base/vim.nix
@@ -147,6 +147,9 @@
           require'nvim-treesitter.configs'.setup {
             highlight = {
               enable = true
+            },
+            indent = {
+              enable = true
             }
           }
           EOF
