ae6361cab2fada713117049763dec6a7540a87c2
Loric Brevet <loric.brevet@amersports.com>
2022-02-28 15:01:18 +0100

    Fixed by https://github.com/nix-community/home-manager/pull/1650/files

--- home.nix
+++ home.nix
@@ -6,9 +6,6 @@
   # Enable settings that make home manager work better on Linux distribs other than NixOS
   targets.genericLinux.enable = true;
 
-  # To fix glibc locale bug (https://github.com/NixOS/nixpkgs/issues/38991)
-  home.sessionVariables.LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-
   imports = [
     ./roles/hm/base
     ./roles/hm/xfce
