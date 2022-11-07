ae372003eef11c26856576804e11b9fd896d558b
Loric Brevet <loric.brevet@gmail.com>
2022-11-07 15:35:56 +0100

    Add indend settings to kakoune

--- roles/hm/base/kakoune.nix
+++ roles/hm/base/kakoune.nix
@@ -6,10 +6,15 @@
 
   programs.kakoune = {
     enable = true;
+
     config = {
       colorScheme = "ansi";
       ui.assistant = "none";
+
+      indentWidth = 4;
+      tabStop = 4;
     };
+
     extraConfig = ''
       def find -params 1 -shell-script-candidates %{ git ls-files } %{ edit %arg{1} }
     '';
