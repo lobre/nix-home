3dc4349df6fd5835e8f754a6e772d8601621f0ea
Loric Brevet <loric.brevet@gmail.com>
2022-11-11 12:36:58 +0100

    Add indentation hooks to kakoune

--- roles/hm/base/kakoune.nix
+++ roles/hm/base/kakoune.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   # colorscheme
@@ -10,20 +10,73 @@
     config = {
       colorScheme = "ansi";
       indentWidth = 4;
-      tabStop = 4;
       showMatching = true;
 
       ui = {
         assistant = "none";
         enableMouse = true;
       };
+
+      hooks = [
+        {
+          name = "BufSetOption";
+          option = "filetype=go";
+          commands = ''
+            set-option buffer indentwidth 0
+            set-option buffer formatcmd '${pkgs.gotools}/bin/goimports | ${config.programs.go.package}/bin/gofmt'
+            hook buffer BufWritePre .* format
+          '';
+        }
+        {
+          name = "BufSetOption";
+          option = "filetype=zig";
+          commands = ''
+            set-option buffer indentwidth 4
+            set-option buffer formatcmd '${pkgs.zig}/bin/zig fmt --stdin'
+            hook buffer BufWritePre .* format
+          '';
+        }
+        {
+          name = "BufSetOption";
+          option = "filetype=elm";
+          commands = ''
+            set-option buffer indentwidth 4
+            set-option buffer formatcmd '${pkgs.elmPackages.elm-format}/bin/elm-format --stdin'
+            hook buffer BufWritePre .* format
+          '';
+        }
+        {
+          name = "BufSetOption";
+          option = "filetype=nix";
+          commands = ''
+            set-option buffer indentwidth 2
+            set-option buffer formatcmd ${pkgs.nixfmt}/bin/nixfmt
+            hook buffer BufWritePre .* format
+          '';
+        }
+        {
+          name = "BufSetOption";
+          option = "filetype=html";
+          commands = ''
+            set-option buffer indentwidth 2
+          '';
+        }
+        {
+          name = "BufSetOption";
+          option = "filetype=json";
+          commands = ''
+            set-option buffer indentwidth 2
+          '';
+        }
+      ];
     };
 
     extraConfig = ''
-      def find -params 1 -shell-script-candidates %{ git ls-files } %{ edit %arg{1} }
+      set-option global startup_info_version 20211108
 
-      # hide changelog on startup
-      set global startup_info_version 20211108
+      add-highlighter global/ wrap
+
+      define-command find -params 1 -shell-script-candidates %{ git ls-files --recurse-submodules } %{ edit %arg{1} }
     '';
   };
 }
