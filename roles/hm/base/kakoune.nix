c41f80ea06286d955ba96ffcdab2f1d9b3744834
Loric Brevet <loric.brevet@gmail.com>
2022-11-18 15:13:57 +0100

    Don’t use home manager syntax for kakoune

--- roles/hm/base/kakoune.nix
+++ roles/hm/base/kakoune.nix
@@ -7,124 +7,73 @@
   programs.kakoune = {
     enable = true;
 
-    config = {
-      colorScheme = "ansi";
-      indentWidth = 4;
-      showMatching = true;
-
-      ui = {
-        assistant = "none";
-        enableMouse = true;
-        setTitle = true;
-      };
-
-      hooks = [
-        {
-          name = "BufSetOption";
-          option = "filetype=go";
-          commands = ''
-            set buffer indentwidth 0
-            set buffer formatcmd '${pkgs.gotools}/bin/goimports | ${config.programs.go.package}/bin/gofmt'
-            hook buffer BufWritePre .* format
-
-            set buffer ctagscmd "ctags -R --fields=+S --languages=go --exclude=testdata --exclude=*test.go --exclude=internal --exclude=cmd"
-            set buffer ctagspaths %sh{ echo ". $GOROOT/src" }
-          '';
-        }
-        {
-          name = "BufSetOption";
-          option = "filetype=zig";
-          commands = ''
-            set buffer indentwidth 4
-            set buffer formatcmd '${pkgs.zig}/bin/zig fmt --stdin'
-            hook buffer BufWritePre .* format
-          '';
-        }
-        {
-          name = "BufSetOption";
-          option = "filetype=elm";
-          commands = ''
-            set buffer indentwidth 4
-            set buffer formatcmd '${pkgs.elmPackages.elm-format}/bin/elm-format --stdin'
-            hook buffer BufWritePre .* format
-          '';
-        }
-        {
-          name = "BufSetOption";
-          option = "filetype=nix";
-          commands = ''
-            set buffer indentwidth 2
-            set buffer formatcmd ${pkgs.nixfmt}/bin/nixfmt
-            hook buffer BufWritePre .* format
-          '';
-        }
-        {
-          name = "BufSetOption";
-          option = "filetype=html";
-          commands = ''
-            set buffer indentwidth 2
-          '';
-        }
-        {
-          name = "BufSetOption";
-          option = "filetype=json";
-          commands = ''
-            set buffer indentwidth 2
-          '';
-        }
-        {
-          name = "WinCreate";
-          option = ".*";
-          commands = "wrap-enable";
-        }
-      ];
-
-      keyMappings = [
-        {
-          mode = "user";
-          key = "y";
-          effect = "<a-|>${pkgs.xsel}/bin/xsel --input --clipboard<ret>";
-          docstring = "yank to clipboard";
-        }
-        {
-          mode = "user";
-          key = "p";
-          effect = "<a-!>${pkgs.xsel}/bin/xsel --output --clipboard<ret>";
-          docstring = "paste after from clipboard";
-        }
-        {
-          mode = "user";
-          key = "P";
-          effect = "!${pkgs.xsel}/bin/xsel --output --clipboard<ret>";
-          docstring = "paste before from clipboard";
-        }
-        {
-          mode = "user";
-          key = "R";
-          effect = "|${pkgs.xsel}/bin/xsel --output --clipboard<ret>";
-          docstring = "replace from clipboard";
-        }
-      ];
-    };
-
     extraConfig = ''
+      # hide changelog on startup
       set global startup_info_version 20211108
+
+      # theme
+      colorscheme ansi
+
+      # default options
+      set global indentwidth 4
+      set global ui_options terminal_set_title=true terminal_assistant=none terminal_enable_mouse=true
       set global autocomplete prompt
 
-      declare-option str-list findcmd git ls-files --recurse-submodules
+      # show matching characters
+      add-highlighter global/ show-matching
+
+      # find and open a git file quickly
+      def find -params 1 -shell-script-candidates "git ls-files --recurse-submodules" %{edit %arg{1}}
+
+      # command to show line numbers
       declare-option str-list numberflags -separator ' ' -hlcursor
-      declare-option str-list listflags -tab '→' -spc '·' -nbsp '␣' -lf '↲'
-
-      def find -params 1 -shell-script-candidates "%opt{findcmd}" %{edit %arg{1}}
-
       def number-enable %{ add-highlighter global/number number-lines %opt{numberflags} }
       def number-disable "remove-highlighter global/number"
 
+      # command to show hidden characters
+      declare-option str-list listflags -tab '→' -spc '·' -nbsp '␣' -lf '↲'
       def list-enable %{ add-highlighter window/list show-whitespaces %opt{listflags} }
       def list-disable "remove-highlighter window/list"
 
+      # command to disable wrapping
       def wrap-enable "add-highlighter window/wrap wrap"
       def wrap-disable "remove-highlighter window/wrap"
+      hook global WinCreate .* "wrap-enable"
+
+      hook global BufSetOption filetype=go %{
+        set buffer indentwidth 0
+        set buffer formatcmd '${pkgs.gotools}/bin/goimports | ${config.programs.go.package}/bin/gofmt'
+        hook buffer BufWritePre .* format
+
+        set buffer ctagscmd "ctags -R --fields=+S --languages=go --exclude=testdata --exclude=*test.go --exclude=internal --exclude=cmd"
+        set buffer ctagspaths %sh{ echo ". $GOROOT/src" }
+      }
+
+      hook global BufSetOption filetype=zig %{
+        set buffer indentwidth 4
+        set buffer formatcmd '${pkgs.zig}/bin/zig fmt --stdin'
+        hook buffer BufWritePre .* format
+      }
+
+      hook global BufSetOption filetype=elm %{
+        set buffer indentwidth 4
+        set buffer formatcmd '${pkgs.elmPackages.elm-format}/bin/elm-format --stdin'
+        hook buffer BufWritePre .* format
+      }
+
+      hook global BufSetOption filetype=nix %{
+        set buffer indentwidth 2
+        set buffer formatcmd ${pkgs.nixfmt}/bin/nixfmt
+        hook buffer BufWritePre .* format
+      }
+
+      hook global BufSetOption filetype=(html|json) "set buffer indentwidth 2"
+
+      # clipboard mappings
+      map global user y "<a-|>${pkgs.xsel}/bin/xsel --input --clipboard<ret>"  -docstring "yank from clipboard"
+      map global user p "<a-!>${pkgs.xsel}/bin/xsel --output --clipboard<ret>" -docstring "paste after from clipboard"
+      map global user P "!${pkgs.xsel}/bin/xsel --output --clipboard<ret>"     -docstring "paste before from clipboard"
+      map global user R "|${pkgs.xsel}/bin/xsel --output --clipboard<ret>"     -docstring "replace from clipboard"
     '';
   };
 }
