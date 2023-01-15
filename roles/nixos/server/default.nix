885111b4a2e79757cbc8f4d1f1155113b717d5d8
Loric Brevet <loric.brevet@gmail.com>
2023-01-15 19:40:26 +0100

    Put all server logic in same nix file

--- roles/nixos/server/default.nix
+++ roles/nixos/server/default.nix
@@ -1,8 +1,32 @@
 { config, pkgs, ... }:
 
+let
+  dataDir = "/var/lib/media";
+in
+
 {
   # 8080 to allow quickly spinning a server for development
   networking.firewall.allowedTCPPorts = [ 22 80 443 8080 ];
+
+  users.users.media = {
+    group = "media";
+    home = "${dataDir}";
+    createHome = true;
+    isSystemUser = true;
+  };
+
+  users.groups.media = { };
+
+  # create directory structure with perms
+  systemd.tmpfiles.rules = [
+    "d '${dataDir}' 0770 media media"
+    "d '${dataDir}/Downloads' 0770 media media"
+    "d '${dataDir}/Music' 0770 media media"
+    "d '${dataDir}/Pictures' 0770 media media"
+    "d '${dataDir}/Videos' 0770 media media"
+    "d '${dataDir}/Videos/Movies' 0770 media media"
+    "d '${dataDir}/Videos/Series' 0770 media media"
+  ];
 
   services = {
     sshd.enable = true;
@@ -35,16 +59,65 @@
         providers = { docker = { exposedbydefault = false; }; };
       };
 
-      dynamicConfigOptions.http.middlewares = {
-        redirect-to-https.redirectScheme = {
-          scheme = "https";
-          port = "443";
-          permanent = true;
+      dynamicConfigOptions.http = {
+        middlewares = {
+          redirect-to-https.redirectScheme = {
+            scheme = "https";
+            port = "443";
+            permanent = true;
+          };
+        };
+
+        routers = {
+          plex = {
+            rule = "Host(`plex.lobre.io`)";
+            entryPoints = [ "web" "websecure" ];
+            service = "plex";
+            middlewares = [ "redirect-to-https" ];
+          };
+
+          deluge = {
+            rule = "Host(`deluge.lobre.io`)";
+            entryPoints = [ "web" "websecure" ];
+            service = "deluge";
+            middlewares = [ "redirect-to-https" ];
+          };
+        };
+
+        services = {
+          plex.loadBalancer.servers = [{ url = "http://127.0.0.1:32400"; }];
+          deluge.loadBalancer.servers = [{ url = "http://127.0.0.1:8112"; }];
         };
       };
     };
-  };
 
-  imports = [ ./users.nix ./plex.nix ./deluge.nix ];
+    plex = {
+      enable = true;
+      user = "media";
+      group = "media";
+      openFirewall = true;
+    };
+
+    deluge = {
+      enable = true;
+      package = pkgs.deluged; # non gtk version
+      web.enable = true;
+      declarative = true;
+      openFirewall = true;
+      user = "media";
+      group = "media";
+
+      # automatically generate a random password in auth.stateful for the local ui
+      authFile = "${config.services.deluge.dataDir}/.config/deluge/auth.stateful";
+
+      config = {
+        allow_remote = true;
+        download_location = "/var/lib/media/Downloads/";
+        stop_seed_at_ratio = true;
+        stop_seed_ratio = 2;
+        enabled_plugins = [ "Label" ];
+      };
+    };
+  };
 }
 
