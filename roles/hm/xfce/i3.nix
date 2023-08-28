1b674c47e4c29a8110d1ad276ec3b46ff1989288
Loric Brevet <loric.brevet@gmail.com>
2023-08-28 16:41:04 +0200

    Remove colors from i3

--- roles/hm/xfce/i3.nix
+++ roles/hm/xfce/i3.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 
 {
-  # TODO:
-  # finish the styling of bars and colors
   xdg.configFile."i3/config".text = ''
     # variables
     set $alt Mod1
@@ -124,28 +122,7 @@
         position bottom
         tray_output none
         i3bar_command ${pkgs.i3}/bin/i3bar --transparency
-
-        colors {
-            background #000000
-            statusline #ffffff
-            separator #666666
-
-            focused_workspace  #4c7899 #285577 #ffffff
-            active_workspace   #333333 #5f676a #ffffff
-            inactive_workspace #333333 #222222 #888888
-            urgent_workspace   #2f343a #900000 #ffffff
-            binding_mode       #2f343a #900000 #ffffff
-        }
     }
-
-    # class                 border  backgr. text    indicator child_border
-    client.focused          #4c7899 #285577 #ffffff #2e9ef4   #285577
-    client.focused_inactive #333333 #5f676a #ffffff #484e50   #5f676a
-    client.unfocused        #333333 #222222 #888888 #292d2e   #222222
-    client.urgent           #2f343a #900000 #ffffff #900000   #900000
-    client.placeholder      #000000 #0c0c0c #ffffff #000000   #0c0c0c
-
-    client.background       #ffffff
 
     # compositor to avoid graphical glitches and allow transparency
     exec --no-startup-id "${pkgs.picom}/bin/picom --fading --fade-in-step=1.0 --fade-out-step=1.0"
